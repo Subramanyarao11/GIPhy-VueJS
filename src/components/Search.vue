4e78bcdaaaac9acda81094acd86d41108ded3825
Subramanyarao11 <subramanya11rao@gmail.com>
2023-01-26 23:18:31 +0530

    fixed search bar!

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,7 @@
 <template>
-<section class="bg-indigo-dark h-50 p-8">
-              <div class="max-w-xl mx-auto">
-                <input class="w-full h-16 px-3 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg" type="search" placeholder="Search...">
+<section class="h-50 p-8">
+              <div class="appearance-none max-w-xl mx-auto">
+                <input class="w-full h-16 border-2 text-white border-slate-400 focus:border-2 focus:border-slate-300 appearance-none bg-transparent px-3 rounded-lg mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg" type="search" placeholder="Type something to search for awesome gifs...">
           </div>
       </section>
 </template>
