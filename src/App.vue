dc369e53a5143f16ba919a5585b8bc74eec173dd
Subramanyarao11 <subramanya11rao@gmail.com>
2023-01-26 21:50:28 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-screen h-screen flex flex-col overflow-hidden bg-hero bg-fixed bg-cover justify-center gap-5">
+     <h1 class="mt-8 bg-gradient-to-br from-white via-[#f5eaef] to-[#5f4a54] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
+      Search and Share <br />
+      GIFs Instantly
+    </h1>
+    <Search />
+  </div>
+</template>
+
+
+<script>
+  import Search from './components/Search.vue'
+export default{
+name: 'App',
+components: {
+	Search
+}}
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,14 @@
+<template>
+<section class="bg-indigo-dark h-50 p-8">
+              <div class="max-w-xl mx-auto">
+                <input class="w-full h-16 px-3 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg" type="search" placeholder="Search...">
+          </div>
+      </section>
+</template>
+
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
